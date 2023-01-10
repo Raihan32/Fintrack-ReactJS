9c95aa16af9f1c424353de414b9572f60975e519
dafahuda <dafahudarifai147@gmail.com>
2023-01-10 14:05:58 +0700

    fix isi artikel dafa 10

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,25 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/home/Home";
 import { Routes, Route } from "react-router-dom";
-import Record from './pages/record/Record';
+import Record from "./pages/record/Record";
 import Login from "./pages/login/Login";
 import Register from "./pages/Register/Register";
 import Artikel from "./pages/Artikel/Artikel";
-import Isiartikel from "./pages/isiartikel/Isiartikel";
+// import Isiartikel from "./pages/isiartikel/Isiartikel";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/fr" element={<Record />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/fa" element={<Artikel />} />
-    <Route path="/isiartikel" element={<Isiartikel />} />
-    </Routes>
-    <Footer />
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fr" element={<Record />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/fa" element={<Artikel />} />
+        {/* <Route path="/isiartikel" element={<Isiartikel />} /> */}
+      </Routes>
+      <Footer />
     </>
   );
 }
