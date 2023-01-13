e8e17d28f8c19e78a57ee559d85cc5c914ffa15a
Dwi Aryani <dwiaryani897@gmail.com>
2023-01-13 11:56:03 +0700

    just added

--- src/pages/course/CourseDetail/courseDetail.jsx
+++ src/pages/course/CourseDetail/courseDetail.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link, useNavigate, useParams, Outlet } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Detail from "../../../constants/DetailCourse";
-import "../CourseDetail/coursedetail.css";
+import React from 'react';
+import { Link, useNavigate, useParams, Outlet } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Detail from '../../../constants/DetailCourse';
+import '../CourseDetail/coursedetail.css';
 
 const CourseDetail = () => {
   const navigate = useNavigate();
@@ -11,23 +11,14 @@ const CourseDetail = () => {
   return (
     <Container className="container-course-detail">
       <div className="course-detail">
-        <img
-          src={Detail[id - 1].image}
-          alt="foto-1"
-          className="course-cover-detail"
-        />
+        <img src={Detail[id - 1].image} alt="foto-1" className="course-cover-detail" />
         <div className="course-card-detail">
           <div className="course-detail-description">
             <p className="course-detail-title">
-              <span style={{ fontWeight: "bold" }}>
-                {Detail[id - 1].heading}
-              </span>
+              <span style={{ fontWeight: 'bold' }}>{Detail[id - 1].heading}</span>
             </p>
             <p className="course-detail-description"></p>
-            <button
-              className="course-button-detail"
-              onClick={() => navigate(-1)}
-            >
+            <button className="course-button-detail" onClick={() => navigate(-1)}>
               BACK
             </button>
             <Link
@@ -46,13 +37,9 @@ const CourseDetail = () => {
 
       <div className="course-detail-course">
         <div className="course-detail-description-course">
-          <h4 style={{ color: "#060F76", fontWeight: "700" }}>
-            Auditing : Konsep Dasar Audit Laporan Keuangan
-          </h4>
+          <h4 style={{ color: '#060F76', fontWeight: '700' }}>Auditing : Konsep Dasar Audit Laporan Keuangan</h4>
           <p>{Detail[id - 1].content_description}.</p>
-          <h5 style={{ color: "#060F76", fontWeight: "700" }}>
-            {Detail[id - 1].title_description}
-          </h5>
+          <h5 style={{ color: '#060F76', fontWeight: '700' }}>{Detail[id - 1].title_description}</h5>
           <p>{Detail[id - 1].content_description1}</p>
         </div>
       </div>
--- src/pages/course/lessonDetail/lesson.jsx
+++ src/pages/course/lessonDetail/lesson.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Container, Breadcrumb } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
-import VideoPlayer from "../../../components/playlist/videoPlayer";
-import { FaRegPauseCircle, FaRegPlayCircle } from "react-icons/fa";
-import Card from "../../../components/Cards/Cards";
-import DetailCourse from "../../../constants/DetailCourse";
-import "./lesson.css";
+import React from 'react';
+import { Container, Breadcrumb } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import VideoPlayer from '../../../components/playlist/videoPlayer';
+import { FaRegPauseCircle, FaRegPlayCircle } from 'react-icons/fa';
+import Card from '../../../components/Cards/Cards';
+import DetailCourse from '../../../constants/DetailCourse';
+import './lesson.css';
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import "swiper/css/bundle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/bundle';
 
 const Lesson = () => {
   return (
@@ -27,94 +27,74 @@ const Lesson = () => {
                 <Breadcrumb.Item active>Course Lesson</Breadcrumb.Item>
               </Breadcrumb>
             </div>
-            <VideoPlayer
-              url={
-                "https://youtu.be/D89Dgg32yLk?list=PLU9il4c3BBUXS1IsXq8FXx-UQFb22Kge5"
-              }
-              style={{ borderRadius: "1rem" }}
-            />
+            <VideoPlayer url={'https://youtu.be/D89Dgg32yLk?list=PLU9il4c3BBUXS1IsXq8FXx-UQFb22Kge5'} style={{ borderRadius: '1rem' }} />
             <div className="deskripsi-video">
-              <h4 style={{ fontWeight: "bold" }}>
-                Penggenalan konsep audit laporan
-              </h4>
+              <h4 style={{ fontWeight: 'bold' }}>Penggenalan konsep audit laporan</h4>
               <p>Auditing : Konsep Dasar Audit Laporan Keuangan</p>
               <details>
                 <summary
                   style={{
                     // display: "block",
-                    display: "inline",
-                    backgroundColor: "#fafafa",
-                    color: "#0C1EEE",
-                    cursor: "pointer",
+                    display: 'inline',
+                    backgroundColor: '#fafafa',
+                    color: '#0C1EEE',
+                    cursor: 'pointer',
                   }}
                 >
                   Deskripsi..
                 </summary>
                 <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Dolorem exercitationem hic tempore ullam qui, molestias
-                  perspiciatis sequi corporis at. A optio nobis rem. Quidem
-                  repudiandae temporibus perferendis adipisci, similique
-                  molestias in vel ullam aliquid, soluta corporis vero sit dolor
-                  ex.
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem hic tempore ullam qui, molestias perspiciatis sequi corporis at. A optio nobis rem. Quidem repudiandae temporibus perferendis adipisci,
+                  similique molestias in vel ullam aliquid, soluta corporis vero sit dolor ex.
                 </p>
               </details>
             </div>
           </div>
           <div className="playlist">
-            <h4 style={{ color: "#030842", fontWeight: "bold" }}>
-              Playlist Video Course
-            </h4>
+            <h4 style={{ color: '#030842', fontWeight: 'bold' }}>Playlist Video Course</h4>
             <section class="video-playlist">
               <div className="list-lesson">
                 <span>
                   <FaRegPlayCircle
                     style={{
-                      fontSize: "26px",
-                      color: "#8F97F9",
+                      fontSize: '26px',
+                      color: '#8F97F9',
                     }}
                   />
                 </span>
-                <span className="title-playlist">
-                  Penggenalan konsep audit laporan
-                </span>
+                <span className="title-playlist">Penggenalan konsep audit laporan</span>
                 <span className="duration-playlist">10.43.00</span>
               </div>
               <div className="list-lesson">
                 <span>
                   <FaRegPauseCircle
                     style={{
-                      fontSize: "26px",
-                      color: "#8F97F9",
+                      fontSize: '26px',
+                      color: '#8F97F9',
                     }}
                   />
                 </span>
-                <span className="title-playlist">
-                  Dasar-dasar keuangan dengan semangat juangan dengan baik
-                  semoga lekas
-                </span>
+                <span className="title-playlist">Dasar-dasar keuangan dengan semangat juangan dengan baik semoga lekas</span>
                 <span className="duration-playlist">10.43.00</span>
               </div>
               <div className="list-lesson">
                 <span>
                   <FaRegPauseCircle
                     style={{
-                      fontSize: "26px",
-                      color: "#8F97F9",
+                      fontSize: '26px',
+                      color: '#8F97F9',
                     }}
                   />
                 </span>
-                <span className="title-playlist">
-                  Penggenalan konsep audit laporan
-                </span>
+                <span className="title-playlist">Penggenalan konsep audit laporan</span>
                 <span className="duration-playlist">10.43.00</span>
               </div>
               <div className="list-lesson">
                 <span>
                   <FaRegPauseCircle
                     style={{
-                      fontSize: "26px",
-                      color: "#8F97F9",
+                      fontSize: '26px',
+                      color: '#8F97F9',
                     }}
                   />
                 </span>
@@ -125,22 +105,20 @@ const Lesson = () => {
                 <span>
                   <FaRegPauseCircle
                     style={{
-                      fontSize: "26px",
-                      color: "#8F97F9",
+                      fontSize: '26px',
+                      color: '#8F97F9',
                     }}
                   />
                 </span>
-                <span className="title-playlist">
-                  Penggenalan konsep audit laporan
-                </span>
+                <span className="title-playlist">Penggenalan konsep audit laporan</span>
                 <span className="duration-playlist">10.43.00</span>
               </div>
               <div className="list-lesson">
                 <span>
                   <FaRegPauseCircle
                     style={{
-                      fontSize: "26px",
-                      color: "#8F97F9",
+                      fontSize: '26px',
+                      color: '#8F97F9',
                     }}
                   />
                 </span>
@@ -151,22 +129,20 @@ const Lesson = () => {
                 <span>
                   <FaRegPauseCircle
                     style={{
-                      fontSize: "26px",
-                      color: "#8F97F9",
+                      fontSize: '26px',
+                      color: '#8F97F9',
                     }}
                   />
                 </span>
-                <span className="title-playlist">
-                  Penggenalan konsep audit laporan
-                </span>
+                <span className="title-playlist">Penggenalan konsep audit laporan</span>
                 <span className="duration-playlist">00.03.00</span>
               </div>
               <div className="list-lesson">
                 <span>
                   <FaRegPauseCircle
                     style={{
-                      fontSize: "26px",
-                      color: "#8F97F9",
+                      fontSize: '26px',
+                      color: '#8F97F9',
                     }}
                   />
                 </span>
@@ -195,25 +171,17 @@ const Lesson = () => {
           navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
           className="mySwiper"
-          style={{ padding: "0rem 1rem 2rem 1rem" }}
+          style={{ padding: '0rem 1rem 2rem 1rem' }}
         >
           {DetailCourse.map((DetailCourse) => (
             <SwiperSlide
               style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
-              <Card
-                key={DetailCourse.id}
-                image={DetailCourse.image}
-                duration={DetailCourse.duration}
-                video={DetailCourse.video}
-                heading={DetailCourse.heading}
-                description={DetailCourse.description}
-                tingkatan={DetailCourse.tingkatan}
-              />
+              <Card key={DetailCourse.id} image={DetailCourse.image} duration={DetailCourse.duration} video={DetailCourse.video} heading={DetailCourse.heading} description={DetailCourse.description} tingkatan={DetailCourse.tingkatan} />
             </SwiperSlide>
           ))}
         </Swiper>
