06016a4fed767b3855159a0c089ab137f82a5ce5
Raihan Fathurrahman <cahtlogosari@gmail.com>
2023-01-19 23:11:06 +0700

    usestate card profile

--- src/components/Profile/Cardprofile.jsx
+++ src/components/Profile/Cardprofile.jsx
@@ -2,9 +2,28 @@ import "./cardprofile.css";
 import Profile from "../Profile/img/fp.png";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import jwtDecode from "jwt-decode";
 
 const Cardprofile = () => {
   const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    refreshToken();
+  }, []);
+
+  const refreshToken = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/token");
+      setToken(response.data.accessToken);
+      const decoded = jwtDecode(response.data.accessToken);
+      setName(decoded.name);
+      setEmail(decoded.email);
+    } catch (error) {}
+  };
 
   const Logout = async () => {
     try {
@@ -38,7 +57,7 @@ const Cardprofile = () => {
       >
         <div className="profile-card">
           <img className="profile-pict" src={Profile} />
-          <div className="profile-nama">Dafa Huda</div>
+          <div className="profile-nama"><p>{name}</p></div>
           <div className="profile-cardprofile">
             <div className="profile-profile">Profile</div>
             <div className="profile-record">Record</div>
@@ -55,12 +74,12 @@ const Cardprofile = () => {
                 <h6 className="profile-pro"> Profile</h6>
               </div>
               <div className="col-sm-9-text-sexondary profile-desc"> Nama </div>
-              <p className="profile-desc1"> Dafa Huda</p>
+              <p className="profile-desc1"> {name}</p>
               <div className="col-sm-9-text-sexondary profile-desc">
                 {" "}
                 Email{" "}
               </div>
-              <p className="profile-desc1"> Dafahuda123@gmail.com</p>
+              <p className="profile-desc1"> {email}</p>
               <div className="col-sm-9-text-sexondary profile-desc">
                 Password
               </div>
